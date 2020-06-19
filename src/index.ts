c442aa6c846cd73d5b4bc3c6bc387a5f2aab54dc
unix <unix.bio@gmail.com>
2020-06-19 15:21:58 +0800

    chore: remove unused

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as commands from './commands'
-import * as options from './options'
-import { Container } from 'func'
-
-const modules = Object.assign({}, commands, options)
-const params = Object.keys(modules).map(key => modules[key])
-
-new Container(params)
